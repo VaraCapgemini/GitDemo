81d7a028a95d91f052e7f3140064b5bb2399cd8f
. <varaprasadgalam.varaprasadgalam@capgemini.com>
2024-06-12 13:04:46 +0530

    my first git example

---
+++ src/com/cg/git/GitDemo.java
@@ -0,0 +1,11 @@
+package com.cg.git;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git demo started");
+
+	}
+
+}
